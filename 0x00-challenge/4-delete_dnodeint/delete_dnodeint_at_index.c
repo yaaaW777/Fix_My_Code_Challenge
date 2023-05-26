4745294bde3987f30d4fc7c68c11a7241501aefa
yaaaW777 <asanteantwi@gmail.com>
2023-05-26 05:55:19 -0700

    updated

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,39 +1,53 @@
-#include <string.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_dnodeint_end - Add a node at the end of a list
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
- * @head: The address of the pointer to the first element of the list
- * @n: The number to store in the new element
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
  *
- * Return: A pointer to the new element
+ * Return: 1 on success, -1 on failure
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *new;
-	dlistint_t *l;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if ((head == NULL) || (*head == NULL))
 	{
-		return (NULL);
+		return (-1);
 	}
-	new->n = n;
-	new->next = NULL;
-	if (*head == NULL)
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
 	{
-		*head = new;
-		new->prev = NULL;
-		return (new);
+		*head = (*head)->next;
+		p++;
 	}
-	l = *head;
-	while (l->next != NULL)
+	if (p != index)
 	{
-		l = l->next;
+		*head = saved_head;
+		return (-1);
 	}
-	l->next = new;
-	new->prev = l;
-	return (new);
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
+	return (1);
 }
